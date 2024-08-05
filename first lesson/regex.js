5af5171651b98725efadf976b99ba1636e74ac0d
Rahul Dandu <rahuldandu08@gmail.com>
2024-08-05 10:25:28 +0530

    added

--- first lesson/regex.js
+++ first lesson/regex.js
@@ -1,4 +1,5 @@
+//this is the email checker and also validation
 const emailChecker = /\w+([.-]?w+)*@\w([.-]?\w+)*(\.\w{2,})+$/;
-
 const email="rahl@gmail.com"
 console.log(emailChecker.test(email))
+

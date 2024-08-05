a828f1b4675eab3ec622c3a3892545113e763af0
Rahul Dandu <rahuldandu08@gmail.com>
2024-08-05 10:43:42 +0530

    added password validator

--- first lesson/regex.js
+++ first lesson/regex.js
@@ -1,5 +1,16 @@
 //this is the email checker and also validation
 const emailChecker = /\w+([.-]?w+)*@\w([.-]?\w+)*(\.\w{2,})+$/;
-const email="rahl@gmail.com"
-console.log(emailChecker.test(email))
+const email = "rahl@gmail.com";
+console.log(emailChecker.test(email));
 
+//password checker
+
+const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const password = "pa4ssword";
+
+if (passwordRegex.test(password) === false) {
+  console.log("password must have one Capital letter and Atleast One Number");
+} else {
+  console.log("password is created succesfully");
+}
+console.log(passwordRegex.test(password))

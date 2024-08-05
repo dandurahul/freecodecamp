f1a420dc6217eb6c8a381bf019c22ea1f9f7905f
Rahul Dandu <rahuldandu08@gmail.com>
2024-08-05 11:26:38 +0530

    added another conditions based on requrements

--- first lesson/regex.js
+++ first lesson/regex.js
@@ -5,20 +5,50 @@ console.log(emailChecker.test(email));
 
 //password checker
 
+// const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+// const password = "pa4ssword";
+
+// if (passwordRegex.test(password) === false) {
+//   console.log("password must have one Capital letter and Atleast One Number");
+// } else {
+//   console.log("password is created succesfully");
+// }
+// console.log(passwordRegex.test(password))
 const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-const password = "pa4ssword";
+const password = "psa4sD";
 
-if (passwordRegex.test(password) === false) {
-  console.log("password must have one Capital letter and Atleast One Number");
-} else {
-  console.log("password is created succesfully");
+const checks = {
+  lowercase: /[a-z]/,
+  uppercase: /[A-Z]/,
+  digit: /\d/,
+  length: /.{8,}/,
+};
+
+let missingConditions = [];
+console.log("something something", !checks.lowercase.test(password));
+if (!checks.lowercase.test(password)) {
+  missingConditions.push("one lowercase letter");
 }
-console.log(passwordRegex.test(password))
+if (!checks.uppercase.test(password)) {
+  missingConditions.push("one uppercase letter");
+}
+if (!checks.digit.test(password)) {
+  missingConditions.push("one digit");
+}
+if (!checks.length.test(password)) {
+  missingConditions.push("at least 8 characters");
+}
+if (missingConditions.length > 0) {
+  console.log("Password must have " + missingConditions);
+} else {
+  console.log("Password is created successfully");
+}
 
+console.log(passwordRegex.test(password));
 
 //userName validator
 
-const userNameRegex=/\w+$/;
-const userNaName="rahul_2Dandu"
+const userNameRegex = /\w+$/;
+const userNaName = "rahul_2Dandu";
 
-console.log("testing the userName",userNameRegex.test(userNaName))
+console.log("testing the userName", userNameRegex.test(userNaName));
